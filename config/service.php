0e1f41ab1271cceaf59b93550278f43bac7bbdd6
devosc <devosc@gmail.com>
2016-11-11 20:40:38 -0600

    tidy up

--- config/service.php
+++ config/service.php
@@ -11,13 +11,13 @@ use Mvc5\Plugin\Service;
 use Mvc5\Plugin\Shared;
 
 return [
-        'view.engines' => new Plugin(View5\Engine\Resolver::class, [new Args([
-        'blade' => new Plugin(View5\Engine\CompilerEngine::class, [new Plugin('view.compiler')]),
+    'template\render' => new Shared('view\factory'),
+    'view\compiler'   => new Plugin(View5\Compiler\Blade::class, [new Args(['cache_dir' => new Param('cache')])]),
+    'view\engines'    => new Plugin(View5\Engine\Resolver::class, [new Args([
+        'blade' => new Plugin(View5\Engine\CompilerEngine::class, [new Plugin('view\compiler')]),
         'php'   => new Plugin(View5\Engine\PhpEngine::class)
     ])]),
-    'view.compiler' => new Plugin(View5\Compiler\Blade::class, [new Args(['cache_dir' => new Param('cache')])]),
-    'view.finder' => new Plugin(View5\Finder\File::class, [new Args([new Param('view')]), new Param('templates')]),
-    'view.factory' => new Service(View5\Factory::class, [new Plugin('view.engines'), new Plugin('view.finder'), new Link]),
-    'template\render' => new Shared('view.factory'),
-    'view\renderer' => [View5\Renderer::class, new Shared('view.factory')]
+    'view\factory'  => new Service(View5\Factory::class, [new Plugin('view\engines'), new Plugin('view\finder'), new Link]),
+    'view\finder'   => new Plugin(View5\Finder\File::class, [new Args([new Param('view')]), new Param('templates')]),
+    'view\renderer' => [View5\Renderer::class, new Shared('view\factory')]
 ];
