1da54190d094f2230cec3d6772803731eff54fe0
devosc <devosc@gmail.com>
2016-11-11 20:40:29 -0600

    add init file for includes

---
+++ init.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ *
+ */
+include __DIR__ . '/src/Compiler/Compile/Directives.php';
+include __DIR__ . '/src/Compiler/Compile/Echos.php';
+include __DIR__ . '/src/Compiler/Compile/Expressions.php';
+include __DIR__ . '/src/Compiler/Compile/Extensions.php';
+include __DIR__ . '/src/Compiler/Compile/Verbatim.php';
+include __DIR__ . '/src/Compiler/Cache.php';
+include __DIR__ . '/src/Compiler/Compiler.php';
+include __DIR__ . '/src/Compiler/Blade.php';
+include __DIR__ . '/src/Engine/ViewEngine.php';
+include __DIR__ . '/src/Engine/PhpEngine.php';
+include __DIR__ . '/src/Engine/CompilerEngine.php';
+include __DIR__ . '/src/Engine/Engine.php';
+include __DIR__ . '/src/Engine/EngineResolver.php';
+include __DIR__ . '/src/Engine/Resolver.php';
+include __DIR__ . '/src/Finder/ViewFinder.php';
+include __DIR__ . '/src/Finder/File.php';
+include __DIR__ . '/src/Finder/Finder.php';
+include __DIR__ . '/src/Template/Loop.php';
+include __DIR__ . '/src/Template/Push.php';
+include __DIR__ . '/src/Template/Render.php';
+include __DIR__ . '/src/Template/Section.php';
+include __DIR__ . '/src/View.php';
+include __DIR__ . '/src/Factory.php';
+include __DIR__ . '/src/Renderer.php';
