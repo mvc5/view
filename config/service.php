c04041dcb2ed8e21efb8361b8986ba75a077178e
devosc <devosc@gmail.com>
2016-11-13 08:28:45 -0600

    tidy up

--- config/service.php
+++ config/service.php
@@ -10,15 +10,14 @@ use Mvc5\Plugin\Plugin;
 use Mvc5\Plugin\Service;
 
 return [
-    'template\render' => new Service(
-        View5\Render::class, [new Plugin('view\resolver'), new Plugin('view\finder'), new Link]
+    'template\render' => new Service(View5\Render::class, [new Plugin('view\resolver'), new Plugin('view\finder'), new Link]
     ),
     'view\compiler' => View5\Compiler\Blade::class,
+    'view\finder' => [View5\Path\FileFinder::class, new Args([new Param('view')]), new Param('templates')],
     'view\resolver' => [View5\Engine\EngineResolver::class, new Args([
         'blade' => new Plugin(
             View5\Engine\CompilerEngine::class, [new Plugin('view\compiler'), new Args(['directory' => new Param('cache')])]
         ),
         'php' => new Plugin(View5\Engine\PhpEngine::class)
-    ])],
-    'view\finder' => [View5\Path\FileFinder::class, new Args([new Param('view')]), new Param('templates')],
+    ])]
 ];
