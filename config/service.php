013a8680a0e4c7fa7a492e100e513831955e03da
devosc <devosc@gmail.com>
2016-11-13 15:52:03 -0600

    remove path finder

--- config/service.php
+++ config/service.php
@@ -10,10 +10,13 @@ use Mvc5\Plugin\Plugin;
 use Mvc5\Plugin\Service;
 
 return [
-    'template\render' => new Service(View5\Render::class, [new Plugin('view\resolver'), new Plugin('view\finder'), new Link]
-    ),
+    'template\render' => new Service(View5\Render::class,[
+        new Plugin('view\resolver'),
+        new Plugin('template\provider'),
+        new Args(['directory' => new Param('view'), 'paths' => new Param('templates')])
+    ]),
+    'template\provider' => new Link,
     'view\compiler' => View5\Compiler\Blade::class,
-    'view\finder' => [View5\Path\FileFinder::class, new Args([new Param('view')]), new Param('templates')],
     'view\resolver' => [View5\Engine\EngineResolver::class, new Args([
         'blade' => new Plugin(
             View5\Engine\CompilerEngine::class, [new Plugin('view\compiler'), new Args(['directory' => new Param('cache')])]
